0e6aa1d09450c60327da415e8e2e86ee33911653
irisviana <vianasantana21@gmail.com>
2021-04-10 10:51:21 -0300

    adding finished GithubApi

--- teste-python-jr-remoto-main/vough_backend/api/integrations/github.py
+++ teste-python-jr-remoto-main/vough_backend/api/integrations/github.py
@@ -6,16 +6,25 @@ class GithubApi:
     API_URL = os.environ.get("GITHUB_API_URL", "https://api.github.com")
     GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
+    def __init__(self):
+        
+        self.url_default = f"{self.API_URL}/orgs/"
+
+
     def get_organization(self, login: str):
         """Busca uma organização no Github
 
         :login: login da organização no Github
         """
-        return {}
+       
+        return requests.get(self.url_default+login)
+
 
     def get_organization_public_members(self, login: str) -> int:
         """Retorna todos os membros públicos de uma organização
 
         :login: login da organização no Github
         """
-        return 0
+        return requests.get(self.url_default+login+"/public_members")
+        
+        
