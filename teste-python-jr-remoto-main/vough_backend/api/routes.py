c0a473158e27a3523f95b6174de649007cee4ebd
irisviana <vianasantana21@gmail.com>
2021-04-10 18:30:47 -0300

    adding basename

--- teste-python-jr-remoto-main/vough_backend/api/routes.py
+++ teste-python-jr-remoto-main/vough_backend/api/routes.py
@@ -4,6 +4,6 @@ from rest_framework.routers import DefaultRouter
 from api import views
 
 routers = DefaultRouter()
-routers.register("orgs", views.OrganizationViewSet)
+routers.register("orgs", views.OrganizationViewSet, basename='Organization')
 
 urlpatterns = [path("", include(routers.urls))]
