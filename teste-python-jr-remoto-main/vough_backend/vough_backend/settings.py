bd2723f508b4c0ff8ab2b187e91b65d367c423b7
irisviana <vianasantana21@gmail.com>
2021-04-14 20:21:26 -0300

    updating configurations

--- teste-python-jr-remoto-main/vough_backend/vough_backend/settings.py
+++ teste-python-jr-remoto-main/vough_backend/vough_backend/settings.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 import django_heroku
-from decouple import config
 from dj_database_url import parse as dburl
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
